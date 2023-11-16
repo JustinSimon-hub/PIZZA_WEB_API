12d5fb3d292cdccf3a6df78ce8f5747334d8b04e
Justin Simon <justinsimon@Justins-MBP.lan>
2023-11-16 09:16:31 -0800

    Complete tutorial for web api using in memory data caching

---
+++ Program.cs
@@ -0,0 +1,30 @@
+//IF HTTPREPL CONNECT DOESNT WORK, USE export PATH="$HOME/.dotnet/tools:$PATH" IN THE TERMINAL TO ENABLE CONNECTION TO THE API
+//the port for this project is 7084.
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
